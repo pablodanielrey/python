b5c52bfd3a6eff4cd15a0a78bf2ce969b40d3a4c
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-05-27 16:48:35 -0300

    agregue el codigo necesario apra migrar los proyectos de soporte. faltan las tareas que estaban contenidas dentro de esos proyectos.

--- redmine/syncIssues.py
+++ redmine/syncIssues.py
@@ -1,6 +1,6 @@
 import uuid
 import psycopg2
-
+from redmine import Redmine
 
 class RProject:
 	def __init__(self, id, parent_id, name, desc):
@@ -8,6 +8,28 @@ class RProject:
 		self.parent_id = parent_id
 		self.name = name
 		self.desc = desc
+
+	def __str__(self):
+		return str(self.id) + " " + self.name + " " + str(self.parent_id)
+ 
+	@staticmethod
+	def read(cur):
+		sql = "select id,name,description,is_public,parent_id,created_on,updated_on,identifier,status,lft,rgt from projects where parent_id = 2"
+		cur.execute(sql);
+		rows = cur.fetchall();
+		projects = []
+		for id,name,desc,is_p,p_id,c_on,u_on,ident,status,lft,rgt in rows:
+			rp = RProject(id,p_id,name,desc)
+			projects.append(rp)
+
+		return projects
+
+
+
+	def write(self,redmine):
+		id = uuid.uuid4().__str__().replace('-','')
+		pj = redmine.project.create(name=self.name, identifier=id, description=self.desc, is_public=True, parent_id=13, inherit_members=True)
+
 
 
 class RIssue:
@@ -71,11 +93,15 @@ class RIssue:
 
 
 try:
-	con2 = psycopg2.connect(database='redmine_default', host='127.0.0.1', port='5433', user='', password='')
-# 	con = psycopg2.connect(database='redmine251', host='127.0.0.1', user='usuario', password='clave')
+	redmine = Redmine("https://redmine-comp.econo.unlp.edu.ar",username="usuario",password="clave", requests={'verify':False})
 
-	cur = con2.cursor();
-	RIssue.read(cur,[])
+	con2 = psycopg2.connect(database='redmine_default', host='127.0.0.1', port='5433', user='', password='')
+	cur = con2.cursor()
+
+	projects = RProject.read(cur)
+	
+	for pr in projects:
+		pr.write(redmine)
 
 	cur.close();
 	con2.close();
