ec24564cceea835d7a013410a868e57be1a9daed
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-11-26 19:00:49 -0300

    agregue mas info a la base

--- dhcp/dhcp-event.py
+++ dhcp/dhcp-event.py
@@ -2,6 +2,8 @@
 import psycopg2
 import os, sys
 
+host = ''
+
 
 #ClientIP, ClientMac, host-decl-name
 if (len(sys.argv) > 1):
@@ -18,32 +20,37 @@ if (len(sys.argv) > 1):
 		f.flush()
 		f.close()
 
-		db = psycopg2.connect(host="127.0.0.1", user="dhcp", passwd="dhcp", dbname="dhcp")
+		db = psycopg2.connect(host=host, user="dhcp", password="dhcp", dbname="dhcp")
                 cursor = db.cursor()
+		cursor.execute("delete from leases where ip = %s",[clientIP])
 		cursor.execute("INSERT INTO leases (ip,mac,hostname) VALUES (%s,%s,%s)", [clientIP,clientMac,hostname])
 #		pp.pprint(cursor.__dict__)
 		cursor.close()
 		db.commit()
 		db.close()
+
 	elif command == "release":
 		f = open("/tmp/leases",'a')
 		s = "Released: %s from %s\n" % (clientIP, hostname)
 		f.write(s)
 		f.flush()
 		f.close()
-		db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_pass, db=mysql_db)
+
+		db = psycopg2.connect(host=host, user="dhcp", password="dhcp", dbname="dhcp")
                 cursor = db.cursor()
 		cursor.execute("DELETE FROM records WHERE content = %s AND name = %s",[clientIP,hostname])
 		#pp.pprint(cursor.__dict__)
 		db.commit()
 		db.close()
+
 	elif command == "expiry":
 		f = open("/tmp/leases",'a')
                 s = "Expired: %s from %s\n" % (clientIP, hostname)
                 f.write(s)
                 f.flush()
                 f.close()
-		db = MySQLdb.connect(host=mysql_host, user=mysql_user, passwd=mysql_pass, db=mysql_db)
+	
+		db = psycopg2.connect(host=host, user="dhcp", password="dhcp", dbname="dhcp")
                 cursor = db.cursor()
 		cursor.execute("DELETE FROM records WHERE content = %s AND name = %s",[clientIP,hostname])
 		#pp.pprint(cursor.__dict__)
