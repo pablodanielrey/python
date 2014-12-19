717325b305b60e6c47f39b6e2390aa331774060a
Pablo Daniel Rey <pablodanielrey@gmail.com>
2014-12-19 13:14:31 -0300

    agregue el que aprueba la creacion de la cuenta.

---
+++ websocket/actions/AprobeCreateRequest.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import json, uuid
+import ActionUtils
+import json
+
+"""
+peticion:
+{
+  "id":"id de la peticion"
+  "action":"aprobeCreateRequest",
+  "session":"id de session obtenido en el login",
+  "reqId":"id de la peticion"
+}
+
+respuesta:  
+{
+  "id":"id de la peticion"
+  O "ok":""
+  O "error":""
+}
+
+"""
+
+
+class AprobeCreateRequestAction:
+
+  def handleAction(self, server, message):
+
+    if message['action'] != 'aprobeCreateRequest':
+      return False
+
+    """ chequeo que exista la sesion, etc """
+    session = message['session']
+
+    pid = message['id']
+    reqId = message['reqId']
+    
+    response = {'id':pid, 'ok':'usuario creado correctamente'}
+    server.sendMessage(json.dumps(response))
+
+    return True
+
+
+
+
+
