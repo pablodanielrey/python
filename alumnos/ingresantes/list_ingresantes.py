b9f861c52574ea6b8cc1f4a113cba166337a9fdd
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-02-26 16:25:28 -0300

    modifique el script para que lo exporte usando ; en vez de , asi no geenra problemas con los nombres.

--- alumnos/ingresantes/list_ingresantes.py
+++ alumnos/ingresantes/list_ingresantes.py
@@ -12,13 +12,15 @@ import uuid
 
 l = ldap.initialize("ldap://127.0.0.1:3389")
 l.protocol_version = ldap.VERSION3
-l.simple_bind("admin", "clave")
+l.simple_bind("cn=admin,dc=econo", "pcucqccp")
 
-result = l.search_s('ou=people,dc=econo',ldap.SCOPE_SUBTREE, '(businessCategory=*)',['givenName','x-dcsys-dni','sn','x-dcsys-mail'])
+result = l.search_s('ou=people,dc=econo',ldap.SCOPE_SUBTREE, '(businessCategory=*)',['givenName','x-dcsys-dni','sn','x-dcsys-mail','x-dcsys-legajo'])
 for dn,r in result:
 	mail = 'no tiene'
 	if 'x-dcsys-mail' in r:
 		mail = r['x-dcsys-mail'][0]
-	print "%s,%s,%s,%s" % (r['x-dcsys-dni'][0],r['sn'][0],r['givenName'][0],mail)
+	if 'x-dcsys-legajo' in r:
+		legajo = r['x-dcsys-legajo'][0]
+	print "%s;%s;%s;%s;%s" % (r['x-dcsys-dni'][0],r['sn'][0],r['givenName'][0],legajo,mail)
 
 l.unbind_s()
