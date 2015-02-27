8dcfa0c510aefd55ae095b8f3b870934166dfe20
Pablo Daniel Rey <pablodanielrey@gmail.com>
2015-02-27 09:06:41 -0300

    agregue un script para los listados de sebastian

---
+++ alumnos/agregar-listados-sebastian/ImportStudents.py
@@ -0,0 +1,48 @@
+'''
+Created on 19/12/2013
+
+@author: pablo
+'''
+
+import csv
+import ldap
+import ldap.modlist as modlist
+import uuid
+
+
+l = ldap.initialize("ldap://127.0.0.1:3389")
+l.protocol_version = ldap.VERSION3
+l.simple_bind("cn=admin,dc=econo", "pcucqccp")
+
+with open('/tmp/listado-sebastian.csv') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for nombre,apellido,dni in reader:
+            print "Nombre : ", nombre
+            print "Apellido : ", apellido
+            print "Dni : ", dni
+
+            suuid = str(uuid.uuid4())
+            dn = 'x-dcsys-uuid=' + suuid + ',ou=people,dc=econo'
+
+            person = {}
+            person['objectClass'] = ['top','person','inetOrgPerson','x-dcsys-entidad','x-dcsys-persona','x-dcsys-posgrado']
+            person['x-dcsys-uuid'] = suuid
+            person['x-dcsys-dni'] = dni
+            person['uid'] = dni
+            person['cn'] = nombre
+            person['givenName'] = nombre
+            person['sn'] = apellido
+            person['userPassword'] = dni[-4:]
+
+            try:
+                result = l.search_s('ou=people,dc=econo',ldap.SCOPE_SUBTREE, '(x-dcsys-dni='+dni+")",['cn','x-dcsys-dni','sn','x-dcsys-uuid','x-dcsys-mail'])
+                print result
+                if result == []:
+                    print "agregando %s " % dni
+                    print person
+                    l.add_s(dn,modlist.addModlist(person))
+
+            except ldap.LDAPError, e:
+                print e
+
+l.unbind_s()
