28e5d9d73404c96021cd702eb20148f5c76cd655
Alejandro Oporto <oportoalejandroa@gmail.com>
2015-03-30 14:06:41 -0300

    Actualizacion del archivo cambiar_usuarios.py

--- jano/cambiar_usuarios.py
+++ jano/cambiar_usuarios.py
@@ -25,51 +25,68 @@ for i,row in enumerate(reader):
     profile_dni = path_profiles + dni
     home_dni = path_home + dni
 
-    # reviso si existe el profile del usuario en el path_profiles
-    if os.path.isdir(profile):
-        os.rename(profile,profile_dni)
-        # si existe le cambiamos el nombre a dni
-        print '-SI, EXISTE el profile '  + usuario + ' y se cambio el nombre a '  + dni
-        print '--------DATOS DEL CSV----------'
-        print 'usuario: ' + usuario
-        print 'dni: ' + dni
-        print 'mail: ' + mail
-        # creo link simbolico en el profile
-        os.symlink(path_profiles, path_profiles + usuario)
-
-        pw = pwd.getpwnam('soporte')
-        uid = pw.pw_uid
-        gid = grp.getgrnam('soporte').gr_gid
-        print 'pw >>>>', pw
-        print 'uid >>>>', uid
-        print 'gid >>>>', gid
-
-        os.chown(profile_dni, 1001, 1001)
-
-        profiles_mod +=1
-
     # reviso si existe el home del usuario en el path_home
     if os.path.isdir(home):
-        os.rename(home,home_dni)
-        # si existe le cambiamos el nombre a dni
-        print '-SI, EXISTE el home '  + usuario + ' y se cambio el nombre a '  + dni
-        print '--------DATOS DEL CSV----------'
-        print 'usuario: ' + usuario
-        print 'dni: ' + dni
-        print 'mail: ' + mail
+        # me fijo si no es un link simbolico
+        if not os.path.islink(home):
+            # le cambiamos el nombre a dni
+            os.rename(home,home_dni)
+            print '-SI, EXISTE el home '  + usuario + ' y se cambio el nombre a '  + dni
+            print '--------DATOS DEL CSV----------'
+            print 'usuario: ' + usuario
+            print 'dni: ' + dni
+            print 'mail: ' + mail
+            print '-------------------------------'
 
-        # creo link simbolico en el home
-        os.symlink(path_home, path_home + usuario)
 
-        pw = pwd.getpwnam('soporte')
-        uid = pw.pw_uid
-        gid = grp.getgrnam('soporte').gr_gid
-        print 'pw >>>>', pw
-        print 'uid >>>>', uid
-        print 'gid >>>>', gid
-        # os.chown(dni, 1001, 1001)
+            # cambio los permisos del directorio del usuario
+            # os.chmod(home_dni, 0777)
 
-        home_mod +=1
+            # me fijo cuales son el gid y uid del usuario,ejemplo soporte
+            uid_home = pwd.getpwnam(dni)[2]
+            gid_home = grp.getgrnam(dni)[2]
+            print 'ID de usuario ', dni, uid_home
+            print 'ID de grupo ', dni, gid_home
+            print home_dni
+            # cambio el propietario de usuario a dni
+            os.chown(home_dni,uid_home,gid_home)
 
+            # creo link simbolico en el home
+            os.symlink(home_dni, path_home + usuario)
+
+            home_mod +=1
+
+    # reviso si existe el profile del usuario en el path_profiles
+    if os.path.isdir(profile):
+        # me fijo si no es un link simbolico
+        if not os.path.islink(profile):
+            # le cambiamos el nombre a dni
+            os.rename(profile,profile_dni)
+            print '-SI, EXISTE el profile '  + usuario + ' y se cambio el nombre a '  + dni
+            print '--------DATOS DEL CSV----------'
+            print 'usuario: ' + usuario
+            print 'dni: ' + dni
+            print 'mail: ' + mail
+            print '-------------------------------'
+            # cambio los permisos del directorio del usuario
+            # os.chmod(profile_dni, 0777)
+
+            # me fijo cuales son el gid y uid del usuario, ejemplo soporte
+            uid_profile = pwd.getpwnam(dni)[2]
+            gid_profile = grp.getgrnam(dni)[2]
+            print 'ID de usuario ', dni, uid_profile
+            print 'ID de grupo ', dni, gid_profile
+            print profile_dni
+            # cambio el propietario de usuario a dni
+            os.chown(profile_dni,uid_profile,gid_profile)
+
+
+            # creo link simbolico en el profile
+            os.symlink(profile_dni, path_profiles + usuario)
+
+            profiles_mod +=1
+
+        print '\n'
+
+print 'homes modificados ', home_mod
 print 'Perfiles modificados ', profiles_mod
-print 'home modificados ', home_mod
